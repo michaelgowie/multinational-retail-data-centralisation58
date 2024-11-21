b6774bba8ce0179590fbf0778ee2adeebd46e69b
Michael Gowie <michaelgowie1@gmail.com>
2024-11-21 13:46:55 +0000

    Adds docstrings

--- data_extraction.py
+++ data_extraction.py
@@ -22,17 +22,36 @@ number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.
 
 
 class DataExtractor:
+    '''
+    This class is the one which extracts the data from various sources and puts it into 
+    dataframes, ready to be cleaned.
+    '''
     def read_rds_table(self,dat_con_inst,table_name):
+        '''
+        This retrieves data from the RDS database and puts it into a dataframe.
+        As input, it takes an instance of the DataBaseConnector class and the name of the 
+        table in the RDS database. It returns a DataFrame contaning the data.
+        '''
         engine = dat_con_inst.init_db_engine()
         df = pd.read_sql_table(table_name, engine)
         return df
     def retrieve_pdf_data(self, url):
+        '''
+        This method retrieves data from a PDF file which can be found at a URL and 
+        stores it in a DataFrame.
+        As input it takes a url and it outputs a DataFrame.'''
         df_list = tabula.read_pdf(url,pages='all')
         card_df = df_list[0]
         for df in df_list[1:]:
             card_df = pd.concat([card_df,df], axis=0)
         return card_df
     def extract_from_s3(self, s3_address = None):
+        '''
+        This method obtains a dataframe containing data which can be found at a specific
+        s3 address.
+        It obtains the data from the s3 address, writes it to a file and then reads it from 
+        this file and puts it into a DataFrame.
+        '''
         s3 = boto3.client('s3')
         response = s3.get_object(Bucket='data-handling-public',Key='products.csv')
         object_stuff = response['Body'].read().decode()
@@ -41,6 +60,10 @@ class DataExtractor:
         df = pd.read_csv('products_write.csv')
         return df
     def extract_dates_from_s3(self):
+        '''
+        This method obtains the dates data from the s3 bucket. As the data arrives in JSON
+        form, the method uses json.loads to put it into a DataFrame.
+        '''
         s3 = boto3.client('s3')
         response = s3.get_object(Bucket='data-handling-public',Key='date_details.json')
         date_json_str = response['Body'].read().decode()
@@ -48,10 +71,17 @@ class DataExtractor:
         date_df = pd.DataFrame(date_json)
         return date_df
     def list_number_of_stores(self, endpoint, header_dict):
+        '''
+        This method uses requests to obtain the number of stores from a given endpoint.
+        As input, it takes the endpoint for the number of stores as well as a dictionary of headers.'''
         response = requests.get(endpoint, headers=header_dict)
         response_json = response.json()
         return response_json['number_stores']
     def retrieve_stores_data(self, endpoint, header):
+        '''
+        This method uses requests to obtain the data for each and every one of the stores.
+        It uses the list_number_of_stors method to inform how many requests.get commands
+        it should run. It stores the result of every requests.get command in a single DataFrame.'''
         num = self.list_number_of_stores(number_of_stores_endpoint, header)
         endpoint_zero = endpoint + '0'
         first_data = requests.get(endpoint_zero,headers=header).json()
